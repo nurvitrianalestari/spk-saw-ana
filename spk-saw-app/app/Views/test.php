be69457f91cbd08bf80f1a41ff4c4cba4cf6e336
nurvitrianalestari <anashodik@gmail.com>
2022-11-01 21:13:25 +0700

    inisialisasi boostrap

---
+++ spk-saw-app/app/Views/test.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="<?= base_url('css/boostrap.min.css') ?>"rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <!-- <script src=" <?= base_url('js/boostrap.min.js')?>"></script>
+  </body>
+</html>
